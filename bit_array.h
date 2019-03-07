138a1a14ebda61d8c3d7620770c0b65ceaf67043
Alexandr Chalupnik <alexandr.chalupnik@gmail.com>
2019-03-07 16:22:30 +0100

    Init commit

---
+++ bit_array.h
@@ -0,0 +1,18 @@
+// bit_array.h
+// Řešení IJC-DU1, příklad a), 7.3.2019
+// Autor: Alexandr Chalupnik
+// Přeloženo: gcc 4.2.1
+
+#ifndef PROJ1_BIT_ARRAY_H
+#define PROJ1_BIT_ARRAY_H
+
+typedef unsigned long bit_array_h[];
+
+#define bit_array_create(jmeno_pole,velikost)
+#define bit_array_alloc(jmeno_pole,velikost)
+#define bit_array_free(jmeno_pole)
+#define bit_array_size(jmeno_pole)
+#define bit_array_setbit(jmeno_pole,index,vyraz)
+#define bit_array_getbit(jmeno_pole,index)
+
+#endif //PROJ1_BIT_ARRAY_H
---
+++ main.c
@@ -0,0 +1,11 @@
+// main.c
+// Řešení IJC-DU1, příklad a), 7.3.2019
+// Autor: Alexandr Chalupnik
+// Přeloženo: gcc 4.2.1
+
+#include <stdio.h>
+#include "bit_array.h"
+
+int main() {
+    return 0;
+}
